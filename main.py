d123c05cd465eb50377b2d336f7342be39869294
noir74 <noir74@mail.ru>
2021-01-06 17:16:07 +0300

    initial commit

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ attributes_list = 'ID,OBJECTID,OBJECTGUID,CHANGEID,NUMBER,OPERTYPEID,PREVID,NEXT
 object_attributes = attributes_list.split(',')
 separator = '<>'
 
-#input_data = read_zip.get_objects('Z:\\garbage\\tmp\\gar\\data\\in\\full\\01\\AS_STEADS_20201231_1ff8acf7-020a-40ea-a68f-6d3110a2311e.XML', 2)
+# input_data = read_zip.get_objects('Z:\\garbage\\tmp\\gar\\data\\in\\full\\01\\AS_STEADS_20201231_1ff8acf7-020a-40ea-a68f-6d3110a2311e.XML', 2)
 read_zip.get_objects('Z:\\garbage\\tmp\\gar\\data\\in\\delta\\gar_delta_xml.zip', 'AS_HOUSES')
-#parse_xml.process_objects(object_type, object_attributes, separator, input_data)
-
-
+# parse_xml.process_objects(object_type, object_attributes, separator, input_data)
