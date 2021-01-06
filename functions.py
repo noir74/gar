d73f6f9fa444efa765fe7dc164d51ce6a663d698
noir74 <noir74@mail.ru>
2021-01-06 18:31:39 +0300

    всё удачно записалось в файл

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ import sys
 def add_to_file(input_zip_file, files_to_fetch, output_plain_file):
     zip_data = zipfile.ZipFile(input_zip_file, 'r')
     file_list = zip_data.filelist
-    output_plain_stream = open(output_plain_file, "a", 8192)
+    output_plain_stream = open(output_plain_file, mode='a+', buffering=8192,encoding='utf-8')
 
     for FileRecord in file_list:
         if re.match('^[0-9]{2}/' + files_to_fetch + '_[0-9]{8}_', FileRecord.filename) != 0:
@@ -18,7 +18,7 @@ def add_to_file(input_zip_file, files_to_fetch, output_plain_file):
             separator = '<>'
 
             input_xml_stream = io.BytesIO(zip_data.read(FileRecord.filename))
-            parse_xml_data(object_type, object_attributes, separator, input_xml_stream)
+            parse_xml_data(object_type, object_attributes, separator, input_xml_stream, output_plain_stream)
 
     output_plain_stream.close()
     zip_data.close()
@@ -41,7 +41,7 @@ def parse_xml_data(object_type, object_attributes, separator, input_xml_stream, 
                     output_string = output_string + attribute_value + separator
 
                 print(output_string)
-                output_plain_stream.writelines(output_string)
+                output_plain_stream.writelines(output_string + '\n')
 
     parser_instance = xml.sax.make_parser()
     parser_instance.setContentHandler(MovieHandler())
